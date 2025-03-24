22ca9f166a0a4a87c853541b17ded2ad400eafa2
kirillmoiseenkov <kmoiseenkov@phoenixit.ru>
2025-03-25 01:53:19 +0400

    Файлы

--- src/main/java/sbp/erp/core/autotestcheck/CoreAutotestCheckApplication.java
+++ src/main/java/sbp/erp/core/autotestcheck/CoreAutotestCheckApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoreAutotestCheckApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CoreAutotestCheckApplication.class, args);
 	}
-
 }
--- src/main/java/sbp/erp/core/autotestcheck/repo/UserProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package sbp.erp.core.autotestcheck.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sbp.erp.core.autotestcheck.entity.UserProfile;
-
-public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
-    UserProfile findByName(String name);
-}
